6fbe92a4a13cc602da1c8c8f1f42836833d816ec
Chris Ross <chris@xorrior.com>
2020-07-10 10:58:39 -0400

    Initial Commit

---
+++ example/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  example
+//
+//  Created by xorrior on 7/10/20.
+//  Copyright © 2020 Example Organization. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ example/AppDelegate.m
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.m
+//  example
+//
+//  Created by xorrior on 7/10/20.
+//  Copyright © 2020 Example Organization. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (weak) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+
+@end
